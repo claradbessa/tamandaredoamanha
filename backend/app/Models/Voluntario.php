df9685929aac544c0bc8a80ca998a07b93b2bcb1
Clara Bessa <clarabessa@outlook.com>
2025-07-16 20:59:57 -0300

    feat(models): cria model Voluntario

---
+++ backend/app/Models/Voluntario.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Voluntario extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'nome',
+        'email',
+        'senha',
+        'ativo',
+    ];
+}
