78b981d9953a63a417166d09b91dc019cdfe9b16
Clara Bessa <clarabessa@outlook.com>
2025-07-16 21:11:44 -0300

    feat(api): implementa criação de voluntários

---
+++ backend/app/Http/Controllers/VoluntarioController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Voluntario;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class VoluntarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'nome' => 'required|string|max:100',
+            'email' => 'required|string|email|max:100|unique:voluntarios',
+            'senha' => 'required|string|min:6',
+            'ativo' => 'sometimes|boolean',
+        ]);
+
+        $validatedData['senha'] = Hash::make($validatedData['senha']);
+
+        $voluntario = Voluntario::create($validatedData);
+
+        return response()->json($voluntario, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
