3b4a49288a4371a2a92fa173c30e4edff338025f
Clara Bessa <clarabessa@outlook.com>
2025-07-25 20:49:54 -0300

    fix(ops): ajustes na configuração do Docker

--- backend/docker/start.sh
+++ backend/docker/start.sh
@@ -7,5 +7,5 @@ chown -R www-data:www-data /run/nginx
 # Ajusta permissões do storage e cache (redundância para garantir)
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 
-# Inicia os serviços
-/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+# Inicia os serviços usando o arquivo de configuração padrão
+/usr/bin/supervisord -c /etc/supervisord.conf
