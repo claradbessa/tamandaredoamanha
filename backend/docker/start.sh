bdb4116919006d0d9ab7eebe7141803a1358949a
Clara Bessa <clarabessa@outlook.com>
2025-07-27 17:35:34 -0300

    fix(ops): otimiza e corrige fluxo de build e inicialização do Docker

--- backend/docker/start.sh
+++ backend/docker/start.sh
@@ -1,11 +1,2 @@
 #!/bin/sh
-
-# Cria o diretório de runtime para o Nginx e ajusta permissões
-mkdir -p /run/nginx
-chown -R www-data:www-data /run/nginx
-
-# Ajusta permissões do storage e cache (redundância para garantir)
-chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
-
-# Inicia os serviços usando o arquivo de configuração padrão
 /usr/bin/supervisord -c /etc/supervisord.conf
