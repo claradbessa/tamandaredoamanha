133c92a33e196da822da3b857e608ad7b3c0b201
Clara Bessa <clarabessa@outlook.com>
2025-07-28 07:57:40 -0300

    fix(ops): remove cache de config do script de inicialização para o Render

--- backend/docker/start.sh
+++ backend/docker/start.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
-
-# Otimiza a configuração e as rotas usando as variáveis de ambiente do Render
-
 set -e
 
 echo "Ajustando permissões..."
-chown -R www-data:www-data storage bootstrap/cache || true
-chmod -R 775 storage bootstrap/cache || true
-
-echo "Cacheando configuração e rotas..."
-php artisan config:cache
-php artisan route:cache
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 
 echo "Rodando migrations..."
 php artisan migrate --force
