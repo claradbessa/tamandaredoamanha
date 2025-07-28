daad1072e367e7781fc7fc684e8c645741c6336b
Clara Bessa <clarabessa@outlook.com>
2025-07-28 08:27:37 -0300

    fix(ops): adiciona laço de espera para migração do banco

--- backend/docker/start.sh
+++ backend/docker/start.sh
@@ -4,8 +4,18 @@ set -e
 echo "Ajustando permissões..."
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 
-echo "Rodando migrations..."
-php artisan migrate --force
+echo "Aguardando o banco de dados ficar pronto..."
+until php artisan migrate --force; do
+  echo "O banco de dados não está pronto, tentando novamente em 3 segundos..."
+  sleep 3
+done
+
+echo "Cacheando configuração e rotas..."
+php artisan config:cache
+php artisan route:cache
+
+echo "Criando o link de armazenamento..."
+php artisan storage:link
 
 echo "Iniciando supervisord..."
 /usr/bin/supervisord -c /etc/supervisord.conf
