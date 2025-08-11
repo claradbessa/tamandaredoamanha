2580cda769737e24efdc1952424276040327f3e4
Clara Bessa <clarabessa@outlook.com>
2025-08-11 19:20:18 -0300

    fix(api): corrige lógica de disco no PostagemService e adiciona rota OPTIONS
    fix(api): corrige lógica de disco no PostagemService e  rotas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,25 +10,10 @@ use App\Http\Controllers\GestorController;
 use App\Http\Controllers\MatriculaController;
 use App\Http\Controllers\PostagemController;
 use App\Http\Controllers\VoluntarioController;
-use Illuminate\Http\Response;
 
 // Rotas Públicas de Autenticação
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-
-// Adiciona uma rota para responder aos pedidos de verificação OPTIONS
-Route::options('/postagens', function () {
-    return response('', Response::HTTP_NO_CONTENT)
-        ->header('Access-Control-Allow-Origin', request()->header('Origin'))
-        ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
-        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-        ->header('Access-Control-Allow-Credentials', 'true');
-});
-
-// Adiciona uma rota para responder aos pedidos de verificação OPTIONS
-Route::options('/postagens/{postagem?}', function () {
-    return response()->noContent();
-});
 
 // Grupo de Rotas Protegidas
 Route::middleware('auth:sanctum')->group(function () {
