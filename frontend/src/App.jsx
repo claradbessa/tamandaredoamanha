e1e0c1bddd3204c1eb3b5f6792c0f5a8035921b0
Clara Bessa <clarabessa@outlook.com>
2025-08-19 15:29:23 -0300

    feat(frontend): cria página pública de ContatoPage e ajusta CardSection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import GaleriaAdminPage from './pages/GaleriaAdminPage';
 import GaleriaPage from './pages/GaleriaPage';
 import BlogPage from './pages/BlogPage';
 import SinglePostPage from './pages/SinglePostPage'; 
+import ContatoPage from './pages/ContatoPage'
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
         <Route path="/galeria" element={<GaleriaPage />} />
         <Route path="/blog" element={<BlogPage />} /> 
         <Route path="/post/:id" element={<SinglePostPage />} />
+        <Route path="/contato" element={<ContatoPage />} />
 
         {/* Rotas Protegidas com o Layout de Admin */}
         <Route element={<ProtectedRoute />}>
--- frontend/src/components/home/CardSection.jsx
+++ frontend/src/components/home/CardSection.jsx
@@ -2,21 +2,29 @@ import { Link } from 'react-router-dom';
 import './CardSection.css';
 
 function CardSection() {
+  const whatsappNumber = '5512996051909';
+  const whatsappLink = `https://wa.me/${whatsappNumber}`;
+
   return (
     <div className="cta-container">
       <div className="cta-card voluntariado">
         <h2>VOLUNTARIADO</h2>
         <p>Quer fazer parte da nossa missão? Embarque com a gente e ajude a transformar vidas.</p>
-        <Link to="/contato" className="cta-button">
+        <a 
+          href={whatsappLink} 
+          className="cta-button"
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
           Faça parte
-        </Link>
+        </a>
       </div>
       <div className="cta-card doe">
         <h2>DOE</h2>
         <p>Sua doação faz a diferença.<br/> Participe da nossa missão e multiplique sorrisos.</p>
-        <Link to="/contato" className="cta-button">
-          Obrigada!
-        </Link>
+        <a to="/contato" className="cta-button">
+          Quero Doar
+        </a>
       </div>
     </div>
   );
