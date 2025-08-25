9d5836453da1f35b43edfdb2ac6053523342140b
Clara Bessa <clarabessa@outlook.com>
2025-08-25 07:56:50 -0300

    fix(deploy): corrige o upload de arquivos para o Cloudinary

--- backend/config/filesystems.php
+++ backend/config/filesystems.php
@@ -62,8 +62,11 @@ return [
         ],
 
         'cloudinary' => [
-            'driver' => 'cloudinary',
-        ],
+        'driver'     => 'cloudinary',
+        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+        'api_key'    => env('CLOUDINARY_API_KEY'),
+        'api_secret' => env('CLOUDINARY_API_SECRET'),
+    ],
 
     ],
 
