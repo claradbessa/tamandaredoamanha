c8406fb0ca7c4674d1203da85201f5929b85c116
Clara Bessa <clarabessa@outlook.com>
2025-08-25 11:05:13 -0300

    fix(config): ajusta a configuração do disco Cloudinary em filesystems

--- backend/config/filesystems.php
+++ backend/config/filesystems.php
@@ -68,6 +68,7 @@ return [
                 'api_key'    => env('CLOUDINARY_API_KEY'),
                 'api_secret' => env('CLOUDINARY_API_SECRET'),
             ],
+            'url' => env('CLOUDINARY_URL'),
         ],
 
     ],
