b64d72f6d3603bca063f56dbdce7c6956ad89fdd
Clara Bessa <clarabessa@outlook.com>
2025-08-25 18:34:51 -0300

    fix(config): corrige a estrutura de configuração do disco Cloudinary

--- backend/config/filesystems.php
+++ backend/config/filesystems.php
@@ -37,8 +37,14 @@ return [
         ],
 
         'cloudinary' => [
-            'driver' => 'cloudinary',
-        ],
+        'driver' => 'cloudinary',
+        'url' => env('CLOUDINARY_URL', sprintf(
+            'cloudinary://%s:%s@%s',
+            env('CLOUDINARY_API_KEY'),
+            env('CLOUDINARY_API_SECRET'),
+            env('CLOUDINARY_CLOUD_NAME')
+        )),
+],
 
     ],
 
