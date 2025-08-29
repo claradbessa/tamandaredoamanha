d82fd0e25da96c26232c138e4e6cb58e23161c51
Clara Bessa <clarabessa@outlook.com>
2025-08-28 21:46:26 -0300

    fix: Adiciona regras de rewrite para SPA na Vercel

--- backend/config/filesystems.php
+++ backend/config/filesystems.php
@@ -4,6 +4,33 @@ return [
 
 <<<<<<< Updated upstream
     'default' => env('FILESYSTEM_DISK', 'local'),
+=======
+    /*
+    |--------------------------------------------------------------------------
+    | Default Filesystem Disk
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the default filesystem disk that should be used
+    | by the framework. The "local" disk, as well as a variety of cloud
+    | based disks are available to your application for file storage.
+    |
+    */
+
+    'default' => env('FILESYSTEM_DISK', 'cloudinary'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Filesystem Disks
+    |--------------------------------------------------------------------------
+    |
+    | Below you may configure as many filesystem disks as necessary, and you
+    | may even configure multiple disks for the same driver. Examples for
+    | most supported storage drivers are configured here for reference.
+    |
+    | Supported drivers: "local", "ftp", "sftp", "s3"
+    |
+    */
+>>>>>>> Stashed changes
 
     'disks' => [
 
